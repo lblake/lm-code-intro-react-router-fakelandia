0b78bb9ad105f28c8dfe07a10743bb3c182a0bb3
lblake <treleven.lloyd@gmail.com>
2023-02-17 14:12:12 +0000

    created client and worked activity1

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './components/home/home';
+import Footer from './components/footer/footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => <footer className="footer">
+    <p>&copy;2023 Site Created By Lloyd Blake.</p>
+</footer>;
+
+export default Footer;
